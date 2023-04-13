68544c3b6506cb4b81f488d023968bd137c4383a
Megan Perry <megan.perry@strongmind.com>
2023-04-12 19:53:43 -0700

    begin creating POST expenses test

--- app/api.rb
+++ app/api.rb
@@ -3,9 +3,15 @@ require 'json'
 
 module ExpenseTracker
   class API < Sinatra::Base
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
+    end
+
     post '/expenses' do
       JSON.generate('expense_id' => 42)
     end
+
     get '/expenses/:date' do
       JSON.generate([])
     end
