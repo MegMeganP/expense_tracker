af62b2848b5374fbd95da6510e2cb552a090eb7c
Megan Perry <megan.perry@strongmind.com>
2023-04-19 19:40:53 -0700

    add tests and functionality for api post responses

--- app/api.rb
+++ app/api.rb
@@ -9,7 +9,15 @@ module ExpenseTracker
     end
 
     post '/expenses' do
-      JSON.generate('expense_id' => 42)
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
     end
 
     get '/expenses/:date' do
